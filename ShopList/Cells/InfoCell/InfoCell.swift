51b672296cb6e50a48aca9a5a5d06cb172e51ad2
a-r-moroz <andrew101520@gmail.com>
2022-08-08 01:46:40 +0300

    add collectionView

---
+++ ShopList/Cells/InfoCell/InfoCell.swift
@@ -0,0 +1,19 @@
+//
+//  InfoCell.swift
+//  ShopList
+//
+//  Created by Andrew Moroz on 8.08.22.
+//
+
+import UIKit
+
+class InfoCell: UICollectionViewCell {
+
+    @IBOutlet weak var infoLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+    }
+
+}
--- ShopList/ViewControllers/iPhone/PremiumModule/PremiumController.swift
+++ ShopList/ViewControllers/iPhone/PremiumModule/PremiumController.swift
@@ -10,11 +10,12 @@ import Lottie
 
 class PremiumController: UIViewController {
     @IBOutlet weak var pageController: UIPageControl!
+    @IBOutlet weak var infoCollection: UICollectionView!
     @IBOutlet weak var headerLabel: UILabel!
-    @IBOutlet weak var infoLabel: UILabel!
     @IBOutlet weak var monthSubButton: UIButton!
     @IBOutlet weak var yearSubButton: UIButton!
     @IBOutlet weak var foreverSubButton: UIButton!
+    @IBOutlet weak var scrollView: UIScrollView!
     
     private var timer: Timer?
     private var counter = 0
@@ -25,6 +26,15 @@ class PremiumController: UIViewController {
         super.viewDidLoad()
         updateLanguage()
         subscribeToNotification()
+        setupCollection()
+        pageController.hidesForSinglePage = true
+        pageController.numberOfPages = infoArray.count
+        setTimer()
+    }
+    
+    override class func awakeFromNib() {
+        super.awakeFromNib()
+
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -34,6 +44,28 @@ class PremiumController: UIViewController {
     override func viewDidDisappear(_ animated: Bool) {
         super.viewDidDisappear(animated)
         NotificationCenter.default.removeObserver(self)
+    }
+    
+    private func setupCollection() {
+        infoCollection.dataSource = self
+        infoCollection.delegate = self
+        infoCollection.register(UINib(nibName: String(describing: InfoCell.self), bundle: nil), forCellWithReuseIdentifier: String(describing: InfoCell.self))
+    }
+    
+    func setTimer() {
+        Timer.scheduledTimer(timeInterval: 4.0, target: self, selector: #selector(updateCollection(sender:)), userInfo: nil, repeats: true)
+    }
+    
+    @objc func updateCollection(sender: Timer) {
+        if counter < infoArray.count - 1 {
+            counter += 1
+            let x = CGFloat(counter) * view.frame.width
+            infoCollection.setContentOffset(CGPoint(x:x, y:0), animated: true)
+        } else {
+            counter = 0
+            let x = CGFloat(counter) * view.frame.width
+            infoCollection.setContentOffset(CGPoint(x:x, y:0), animated: true)
+        }
     }
     
     @objc  func updateLanguage() {
@@ -48,4 +80,30 @@ class PremiumController: UIViewController {
         NotificationCenter.default.addObserver(self, selector: #selector(updateLanguage), name: .languageChange, object: nil)
     }
 
+}
+
+extension PremiumController: UICollectionViewDataSource, UICollectionViewDelegate, UICollectionViewDelegateFlowLayout {
+    
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return infoArray.count
+    }
+
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: String(describing: InfoCell.self), for: indexPath)
+        guard let infoCell = cell as? InfoCell else { return cell }
+        infoCell.infoLabel.text = infoArray[counter]
+        return infoCell
+    }
+
+    func collectionView(_ collectionView: UICollectionView, willDisplay cell: UICollectionViewCell, forItemAt indexPath: IndexPath) {
+        self.pageController.currentPage = indexPath.section
+    }
+
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: view.frame.width, height: view.frame.height)
+    }
 }
